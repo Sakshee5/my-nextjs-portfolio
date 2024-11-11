31a9d9d864f840fc5c8dc596692d3409a14fb725
Sakshee Patil <saksheepatil05@gmail.com>
2024-11-11 16:59:47 -0500

    trial changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <section className="text-center">
       {/* Profile Image */}
-      <div className="mb-6">
+      <div className="mb-3">
         <Image
           src="/profile.jpeg"
           alt="Profile photo"
@@ -17,12 +17,12 @@ export default function Page() {
       </div>
 
       {/* Name and Title */}
-      <h2 className="text-xl font-medium text-gray-600 mb-4">
+      <h2 className="text-xl font-medium text-gray-400 mb-2">
         Graduate Student | Duke University
       </h2>
 
       {/* Program & Field */}
-      <h3 className="text-lg font-light text-gray-500 mb-8">
+      <h3 className="text-lg font-light text-gray-400 mb-4">
         Artificial Intelligence
       </h3>
 
@@ -33,7 +33,7 @@ export default function Page() {
           Hey! I’m <strong>Sakshee</strong>, an AI Engineer with 2+ years building Generative AI solutions at Deloitte, specializing in multi-agent LLM systems and RAG. Currently pursuing my master’s in{" "}
           <a href="https://ai.meng.duke.edu/degree" target="_blank" className="text-blue-600 hover:underline">
             AI
-          </a> at Duke, with a focus on xAI for interpretable systems.
+          </a> at Duke, with a focus on Explainable AI for interpretable systems.
         </p>
       </div>
     </section>
